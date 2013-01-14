d89b3aeebc992995ef0194f43fa6043e3c534f49
Eric Charles <eric@aos.io>
2013-01-14 06:54:30 +0000

    Automatic message for commit of Mon 14 Jan 2013 06:54:29 UTC

--- src/main/java/aos/edmond/ForSpl.java
+++ src/main/java/aos/edmond/ForSpl.java
@@ -7,7 +7,7 @@ public class ForSpl {
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         for (int i = 0; i <= 20; i++) {
             AosConsole.println(i);
