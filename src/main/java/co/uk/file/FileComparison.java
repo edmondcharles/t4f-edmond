9563ad82f6933b5bd8cda89d6905f6460e0dd8af
Eric Charles <eric@aos.io>
2013-06-23 20:17:17 +0100

    Automatic message for commit of Sun 23 Jun 2013 19:17:17 UTC

---
+++ src/main/java/co/uk/file/FileComparison.java
@@ -0,0 +1,44 @@
+package co.uk.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import aos.console.AosConsole;
+
+public class FileComparison {
+
+    public static void main(String[] args) throws Exception {
+  
+        File file = new File("my simple - file. txt");
+        
+        String fileContent = " bla bla , I am the content";
+        
+        write(file,fileContent);
+        
+        String readFileContent = read(file);
+        
+        if( ! readFileContent.equals (fileContent)){
+            throw new Exception ("NO ...");
+        }
+        else{
+            AosConsole.print("Good, the read content =" + readFileContent);
+        }
+
+    }
+
+    private static String read(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        return br.readLine();
+    }
+
+    private static void write(File file, String fileContent) throws IOException {
+        FileWriter fileWriter = new FileWriter (file);
+        fileWriter.write(fileContent);
+        fileWriter.close();
+    }
+
+}
