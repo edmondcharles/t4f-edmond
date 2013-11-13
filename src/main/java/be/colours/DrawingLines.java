7268f2dfd80aaba98cf267a7a2963da8368603c6
Eric Charles <eric@aos.io>
2013-11-13 18:33:04 +0100

    Automatic message for commit of Wed Nov 13 17:33:04 UTC 2013

---
+++ src/main/java/be/colours/DrawingLines.java
@@ -0,0 +1,27 @@
+package be.colours;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class DrawingLines extends Applet {
+
+    private int width;
+    private int height;
+        
+    public void init(){
+        width=getSize().width;
+        height=getSize().height;
+        setBackground(Color.BLACK);
+    }
+    
+    public void paint(Graphics graphics){
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(0, 0, width, height);
+        for(int i =0; i<10; i++){
+            graphics.drawLine(width, height, i*width/10, 0);
+            
+        }
+    }
+
+}
